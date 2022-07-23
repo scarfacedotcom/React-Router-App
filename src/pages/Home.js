bbe348430d1257d3042053e617c9c504ecd4a8f0
scarfacedotcom <peteradaaku@gmail.com>
2022-07-23 21:49:20 +0100

    few changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,25 @@
+import { useFetch } from "../hooks/useFetch"
+
+//styles
+import "./Home.css"
+
+
 export default function Home() {
+
+  const { data:articles, isPending, error } = useFetch('http://localhost:3000/articles')
+
   return (
-    <div>
-      <h2>Home Page</h2>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum veritatis quibusdam tenetur necessitatibus excepturi maiores delectus veniam, nulla explicabo quos?</p>
+    <div className="home" >
+      <h2>Articles</h2>
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div> }
+      {articles && articles.map(article => (
+        <div key={article.id} className="card" >
+          <h3>{article.title}</h3>
+          <p>{article.author}</p>
+        </div>
+      ))}
+      
     </div>
   )
 }
